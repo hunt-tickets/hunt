36b80b074e5f6900e1103b865e99d829c500cfb9
hunt-tickets <hunt-tickets@github.com>
2026-01-02 10:43:24 -0500

    update.

--- app/api/mercadopago/route.ts
+++ app/api/mercadopago/route.ts
@@ -150,7 +150,8 @@ export async function POST(request: Request) {
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${process.env.SUPABASE_SECRET_KEY}`,
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY}`,
+          apikey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ order_id: order.order_id }),
